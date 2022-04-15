f6b6fa06cf8165bf9a1470011afbd203a7fc443e
Dejan21 <jangelovski_dejan@yahoo.com>
2022-04-15 19:10:53 +0200

    Start bootstrap

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+
+@Input("Second_card")
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
